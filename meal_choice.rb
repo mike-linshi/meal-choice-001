9bfd77429b5748b3c2aae20a436673d2d316365c
mike-linshi <mike.linshi@flatironschool.com>
2015-10-09 00:00:24 -0400

    Done.

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,9 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
+
+def meal_choice(meal = "meat")
+  meal
+  puts meal
+end
+
+meal_choice("chicken")
